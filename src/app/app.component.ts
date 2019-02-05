cf7cc4804efe215314dbfff3d9bd1195241ffb39
admin <surjeet.bhadauriya@techifysolutions.com>
2019-02-05 11:40:12 +0530

    stable

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,9 @@
-import {Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { Meta } from '@angular/platform-browser';
+
 
 @Component({
   selector: 'app-root',
@@ -39,6 +44,30 @@ import {Component} from '@angular/core';
     }
   `]
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
 
+  constructor(private http: HttpClient, private meta: Meta) { }
+
+  ngOnInit() {
+    console.log('hererer');
+    this.getAll()
+    .subscribe(res => {
+      this.meta.addTag({ name: 'description', content: res.aboutUs.replace(/<[^>]*>/g, '') }, true);
+      this.meta.addTag({ name: 'keyword', content: res.keywords.join(',') }, true);
+      this.meta.addTag({ name: 'title', content: `${res.storeName} Shop` }, true);
+      this.meta.addTag({ name: 'twitter:title', content: `${res.storeName} Shop` }, true);
+      this.meta.addTag({ name: 'twitter:description', content: res.aboutUs.replace(/<[^>]*>/g, '') }, true);
+      this.meta.addTag({ name: 'google-site-verification', content: res.googleCode }, true);
+    }, error => {
+    });
+  }
+
+  getAll(): Observable<any> {
+    return this.http.
+    get(`https://develop.new-backend.trackwalkins.com/v3/businesses/get-store-config-public?ePageDomain=http://suristore.wlkn.in`).pipe(
+      map(res => {
+        return res;
+      })
+    );
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -44,16 +44,16 @@ export class HomeComponent implements OnInit {
   constructor(private http: HttpClient, private meta: Meta) { }
 
   ngOnInit() {
-    this.getAll()
-    .subscribe(res => {
-      this.meta.addTag({ name: 'description', content: res.aboutUs.replace(/<[^>]*>/g, '') }, true);
-      this.meta.addTag({ name: 'keyword', content: res.keywords.join(',') }, true);
-      this.meta.addTag({ name: 'title', content: `${res.storeName} Shop` }, true);
-      this.meta.addTag({ name: 'twitter:title', content: `${res.storeName} Shop` }, true);
-      this.meta.addTag({ name: 'twitter:description', content: res.aboutUs.replace(/<[^>]*>/g, '') }, true);
-      this.meta.addTag({ name: 'google-site-verification', content: res.googleCode }, true);
-    }, error => {
-    });
+    // this.getAll()
+    // .subscribe(res => {
+    //   this.meta.addTag({ name: 'description', content: res.aboutUs.replace(/<[^>]*>/g, '') }, true);
+    //   this.meta.addTag({ name: 'keyword', content: res.keywords.join(',') }, true);
+    //   this.meta.addTag({ name: 'title', content: `${res.storeName} Shop` }, true);
+    //   this.meta.addTag({ name: 'twitter:title', content: `${res.storeName} Shop` }, true);
+    //   this.meta.addTag({ name: 'twitter:description', content: res.aboutUs.replace(/<[^>]*>/g, '') }, true);
+    //   this.meta.addTag({ name: 'google-site-verification', content: res.googleCode }, true);
+    // }, error => {
+    // });
   }
 
   getAll(): Observable<any> {
