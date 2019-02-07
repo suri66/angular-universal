c0f0c64fe914e161b5d24008dcb1ee565435c482
Rasul Diwan <rasuldiwan@github.com>
2019-02-07 11:34:50 +0530

    Add Scroll Confiuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,42 +7,8 @@ import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
-  template: `
-  <div class="app-container">
-    <h1>Memco Mobile Shop</h1>
-    <nav class="nav-links">
-      <a routerLink="/">Home</a>
-      <a routerLink="/about">About</a>
-      <a routerLink="/contact">Contact</a>
-    </nav>
-    <div class="router-container">
-      <router-outlet></router-outlet>
-    </div>
-  </div>
-  `,
-  styles: [`
-    :host {
-      background: #f1f1f1;
-      font-family: Roboto,"Helvetica Neue Light","Helvetica Neue",Helvetica,Arial,"Lucida Grande",sans-serif;
-      font-display: swap;
-    }
-    .nav-links {
-      background: #008591;
-    }
-    .nav-links a {
-      color: #fff;
-      display: inline-block;
-      padding: 1rem;
-      margin-right: 3rem;
-      text-decoration: none;
-      font-weight: bold;
-      letter-spacing: 0.1rem;
-    }
-    .router-container {
-      border: 0.5rem #00afc4 solid;
-      padding: 2rem;
-    }
-  `]
+  styleUrls:['app.component.css'],
+  templateUrl:'app.component.html'
 })
 export class AppComponent implements OnInit {
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import {AppComponent} from './app.component';
 import {HomeComponent} from './home/home.component';
 import {TransferHttpCacheModule} from '@nguniversal/common';
 import { HttpClientModule } from '@angular/common/http';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent,
@@ -13,6 +14,7 @@ import { HttpClientModule } from '@angular/common/http';
   ],
   imports: [
     HttpClientModule,
+    SharedModule,
     BrowserModule.withServerTransition({appId: 'my-app'}),
     RouterModule.forRoot([
       { path: '', component: HomeComponent, pathMatch: 'full'},
